fc93677c294d96d136f9975850793a1a48290a1e
sheen <695493298@qq.com>
2018-07-03 00:50:24 +0800

    上传10-11课教程相关代码

---
+++ UnityProject/Assets/Exps/EXP11/FGUIEventSystem.cs
@@ -0,0 +1,46 @@
+﻿using FairyGUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FGUIEventSystem : MonoBehaviour {
+    private string eventID = "10001";
+	// Use this for initialization
+	void Start () {
+        EventListener test10001 = new EventListener(Stage.inst, eventID);
+
+        GRoot.inst.SetContentScaleFactor(1136, 640);
+        Stage.inst.onClick.Add(OnClick);
+        Stage.inst.onClick.Add(OnClick1);
+        //         Stage.inst.AddEventListener(eventID, (EventContext context) => 
+        //         {
+        //             Debug.LogError("收到了消息：" + eventID);
+        //         });
+
+        test10001.Add((EventContext context) =>
+                 {
+                     Debug.LogError("收到了消息：" + eventID+ "context.data="+ context.data);
+                 });
+
+        //         Stage.inst.onClick.Add(OnClick);
+        //         Stage.inst.onClick.Set(OnClick1);
+        //Stage.inst.AddEventListener("onClick", OnClick);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void OnClick(EventContext context)
+    {
+        Stage.inst.DispatchEvent(eventID,"123");
+        //context.StopPropagation();
+        Debug.LogError("点击了Stage");
+    }
+    void OnClick1(EventContext context)
+    {
+        Debug.LogError("点击了StageOnClick1"+ context.sender);
+    }
+}
---
+++ UnityProject/Assets/Exps/Exp10/EXP10Ctrl.cs
@@ -0,0 +1,31 @@
+﻿using FairyGUI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EXP10Ctrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Util.LoadAssets(new List<string>() { "MainPack.bytes",
+            "MainPack@atlas_mvw21n.png",
+            "MainPack@sprites.bytes",
+            "MainPack@atlas0.png" }, Create);
+
+    }
+	
+	// Update is called once per frame
+	void Create () {
+        Util.AddFGUIPack("MainPack");
+        GObject go = Util.CreateComAddToRoot("MainPack", "Exp10Ctrl");
+        Controller c1 = go.asCom.GetController("c1");
+        Controller c2 = go.asCom.GetController("c2");
+        c1.selectedIndex = 0;
+        //c2.selectedIndex = 0;
+        c2.selectedIndex = 1;
+        //c1.selectedIndex = 1;
+        //c2.selectedIndex = 0;
+        //c2.selectedIndex = 1;
+    }
+}
